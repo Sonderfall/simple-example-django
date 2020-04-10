8fd14299069e5c64875b714ba6f5b19c1d46e7e4
Valentin <valentin.macheret.fr@gmail.com>
2020-04-10 12:26:10 +0200

    Comments

--- simpleexampledjango/app/views.py
+++ simpleexampledjango/app/views.py
@@ -4,6 +4,7 @@ from rest_framework import viewsets
 from .models import User
 from .serializers import UserSerializer
 
+# Will do CRUD base operations
 class UserView(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
